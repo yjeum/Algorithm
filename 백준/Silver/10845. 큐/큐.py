f9c04c85766f081eaac2ae861d999bf933a08493
yjeum <139303917+yjeum@users.noreply.github.com>
2023-09-23 20:26:41 +0900

    [Silver IV] Title: 큐, Time: 76 ms, Memory: 34128 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,37 @@
+import sys
+from collections import deque
+
+N = int(sys.stdin.readline())
+
+q = deque()
+for i in range(N):
+    command = sys.stdin.readline().split()
+    if command[0] == 'push':
+        q.append(int(command[1]))
+    
+    elif command[0] == 'pop':
+        if q:
+            print(q.popleft())
+        else:
+            print(-1)
+
+    elif command[0] == 'size':
+        print(len(q))
+
+    elif command[0] == 'empty':
+        if q:
+            print(0)
+        else:
+            print(1)
+
+    elif command[0] == 'front':
+        if q:
+            print(q[0])
+        else:
+            print(-1)
+
+    elif command[0] == 'back':
+        if q:
+            print(q[-1])
+        else:
+            print(-1)
