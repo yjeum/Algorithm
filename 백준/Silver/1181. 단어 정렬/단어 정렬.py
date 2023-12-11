7c567c2e4fd33054179009d665f74b530566b06b
yjeum <139303917+yjeum@users.noreply.github.com>
2023-12-12 00:12:20 +0900

    [Silver V] Title: 단어 정렬, Time: 3468 ms, Memory: 114248 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -7,6 +7,8 @@ word_list = []
 
 for i in range(N):
     word = input().strip()
+    # 지금은 튜플을 사용하여 풀었지만, 
+    # 길이를 키값으로 가진 딕셔너리로 풀어도 좋을것 같음
     if (len(word), word) not in word_list:
         word_list.append((len(word), word))
 
