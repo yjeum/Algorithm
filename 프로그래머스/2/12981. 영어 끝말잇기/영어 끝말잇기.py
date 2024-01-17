087c8616441380bc00287fccaceb8e8a1300d7ca
yjeum <139303917+yjeum@users.noreply.github.com>
2024-01-17 21:28:40 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.16 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -0,0 +1,16 @@
+def solution(n, words):
+    cnt, person = 0,0
+
+    used = [words[0]]
+    before = words[0]
+    
+    for i in range(1,len(words)):
+        if words[i] not in used and words[i][0] == before[-1]:
+            used.append(words[i])
+            before = words[i]
+        else:
+            cnt = (i%n)+1
+            person = (i//n)+1
+            break
+    
+    return [cnt, person]
