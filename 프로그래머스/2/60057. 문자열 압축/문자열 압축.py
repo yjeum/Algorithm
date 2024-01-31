bc324936e3d587e2f604ec4b07ce1e7b87f4538e
yjeum <139303917+yjeum@users.noreply.github.com>
2024-01-31 21:57:08 +0900

    [level 2] Title: 문자열 압축, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -1,25 +1,3 @@
 def solution(s):
-    # 문자열 한글자씩 입력받기
-    answer_lst = []
-    
-    # 1부터 문자열 길이만큼 잘라서 비교
-    for i in range(1, len(s)+1):
-        temp = ''
-        # 첫 비교대상 지정
-        before = s[:i]
-        cnt = 1
-
-        for j in range(i, len(s)+i, i):
-            now = s[j:j+i]
-
-            if before == now:
-                cnt += 1
-            else:
-                if cnt != 1:
-                    temp = temp + str(cnt) + str(before)
-                else:
-                    temp = temp + str(before)
-                before = now
-                cnt = 1
-        answer_lst.append(len(temp))           
-    return min(answer_lst)
+    answer = 0
+    return answer
