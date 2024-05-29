ea9aff014d6c02562175e13d64406962d30bcf99
yjeum <139303917+yjeum@users.noreply.github.com>
2024-05-29 17:54:09 +0900

    [Silver I] Title: 숨바꼭질, Time: 148 ms, Memory: 115640 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,25 +1,29 @@
 import sys
-input = sys.stdin.readline
 from collections import deque
 
+input = sys.stdin.readline
 
-def bfs(start, end):
+def BFS(N, K):
+    q = deque([(N, 0)])
+    # visited를 통해 메모리 관리
+    # +1, -1은 서로 번갈아 진행되기 때문에 방문 관리가 필요
     visited = [0] * 100001
-    q = deque()
-    q.append((start, 0))
+    visited[N] = 1
 
     while q:
-        current, c_cnt = q.popleft()
+        now, cnt = q.popleft()
 
-        if current == end:
-            return c_cnt
+        if now == K:
+            return cnt
+        
+        movement = [now + 1, now - 1, now * 2]
+        for next_m in movement:
+            # 확인 순서 중요
+            # visited 먼저 확인 할 경우 index_error 발생
+            if 0 <= next_m <= 100000 and visited[next_m] == 0:
+                q.append((next_m, cnt + 1))
+                visited[next_m] = 1
 
-        operations = [current + 1, current - 1, current * 2]
-        for operation in operations:
-            if 0 <= operation <= 100000 and visited[operation] == 0:
-                q.append((operation, c_cnt + 1))
-                visited[operation] = 1
+N, K = map(int, input().split())
 
-
-start, end = map(int, input().split())
-print(bfs(start, end))
+print(BFS(N, K))
