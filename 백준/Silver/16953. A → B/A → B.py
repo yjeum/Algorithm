dac0705ab145886a6fe152b7eb9725cf2b912f31
yjeum <139303917+yjeum@users.noreply.github.com>
2024-08-22 14:32:04 +0900

    [Silver II] Title: A → B, Time: 100 ms, Memory: 34212 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.py
+++ 백준/Silver/16953. A → B/A → B.py
@@ -7,17 +7,17 @@ input = sys.stdin.readline
 def makeB(A):
     while q:
         cnt, num = heapq.heappop(q)
-        for temp in [int(num + "1"), int(num) * 2]:
-            if temp > int(B):
+        for temp in [num * 10 + 1, num * 2]:
+            if temp > B:
                 continue
-            elif temp < int(B):
-                heapq.heappush(q, (cnt + 1, str(temp)))
+            elif temp < B:
+                heapq.heappush(q, (cnt + 1, temp))
             else:
                 return cnt + 1
     return -1
 
 
-A, B = input().split()
+A, B = map(int, input().split())
 
 q = [(1, A)]
 
