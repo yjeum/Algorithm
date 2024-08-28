fabf9a80ef7aeee056d37216211a78368aa9b2ab
yjeum <139303917+yjeum@users.noreply.github.com>
2024-08-28 14:50:51 +0900

    [Silver I] Title: 스티커, Time: 732 ms, Memory: 48904 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.py
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -3,18 +3,16 @@ input = sys.stdin.readline
 
 T = int(input())
 for tc in range(1, T + 1):
-    n = int(input())
+    N = int(input())
     stickers = [list(map(int, input().split())) for _ in range(2)]
-    dp = [[0] * n for _ in range(2)]
+    dp = [[0] * N for _ in range(2)]
 
-    # 초기값 세팅
     dp[0][0], dp[1][0] = stickers[0][0], stickers[1][0]
-    if n > 1:
+    if N > 1:
         dp[0][1] = stickers[0][1] + dp[1][0]
         dp[1][1] = stickers[1][1] + dp[0][0]
         
-    # for문 순회하며 최대값 찾기
-    for i in range(2, n):
+    for i in range(2, N):
         dp[0][i] = stickers[0][i] + max(dp[1][i - 1], dp[1][i - 2])
         dp[1][i] = stickers[1][i] + max(dp[0][i - 1], dp[0][i - 2])
-    print(max(dp[0][n - 1], dp[1][n - 1]))
+    print(max(dp[0][N - 1], dp[1][N - 1]))
