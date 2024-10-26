bade20345c74d71ce88852ac3509de6f4e4ce30e
YeJin Eum <139303917+yjeum@users.noreply.github.com>
2024-10-26 14:43:57 +0900

    [Silver IV] Title: 아~파트 아파트, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/31797. 아～파트 아파트/아～파트 아파트.py
+++ 백준/Silver/31797. 아～파트 아파트/아～파트 아파트.py
@@ -1,22 +1,13 @@
-from collections import deque
+import sys
 
-# 입력 처리
-N, M = map(int, input().split())
-hands = deque()
+input = sys.stdin.readline
 
-# 각 참가자의 손 높이를 큐에 추가 (높이, 참가자 번호)
-for i in range(M):
-    H1, H2 = map(int, input().split())
-    hands.append((H1, i + 1))
-    hands.append((H2, i + 1))
+H, N = map(int, input().split())
+hands = []
 
-# 손 높이 기준으로 정렬
-hands = deque(sorted(hands))
-
-# N번 반복하여 가장 아래 손을 위로 올리기
-for _ in range(N):
-    hand = hands.popleft()  # 가장 아래 손을 꺼내서
-    hands.append(hand)      # 맨 위에 다시 쌓기
-
-# N번째로 쌓인 손의 참가자 번호 출력
-print(hands[-1][1])
+for i in range(1, N + 1):
+    h1, h2 = map(int, input().split())
+    hands.append((h1, i))
+    hands.append((h2, i))
+hands.sort()
+print(hands[(H - 1) % (N * 2)][1])
