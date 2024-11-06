04b261937677fe0b1fb3e31a23c5b976645b714d
YeJin Eum <139303917+yjeum@users.noreply.github.com>
2024-11-06 17:50:55 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 2380 ms, Memory: 34168 KB -BaekjoonHub

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
@@ -1,31 +1,27 @@
 import sys
+from collections import deque
 
 input = sys.stdin.readline
 
 N, K = map(int, input().split())
-belt = list(map(int, input().split()))
-robots = [0] * (N)
+belt = deque(map(int, input().split()))  # 벨트를 deque로 변경
+robots = deque([0] * N)  # 로봇도 deque로 변경
 
-start = 0
 cnt = 0
-zero = 0
 
 while True:
     # 0. 횟수 증가
     cnt += 1
 
-    # 1. 벨트 회전
-    start = (start - 1) % (2 * N)
-
-    # 1-2. 벨트 회전에 따른 로봇 이동
-    robots = [0] + robots[:-1]
-    robots[N - 1] = 0
+    # 1. 벨트 회전 (deque.rotate(1) 사용)
+    belt.rotate(1)  # 벨트를 한 칸 회전
+    robots.rotate(1)  # 로봇도 한 칸 회전
+    robots[N-1] = 0
 
     # 2. 로봇 이동
     for cur in range(N - 1, 0, -1):
-        # 벨트의 내구도, 현재 위치의 로봇 여부 확인
         if (
-            belt[(start + cur) % (2 * N)] > 0
+            belt[cur] > 0
             and robots[cur] == 0
             and robots[cur - 1] == 1
         ):
@@ -33,14 +29,15 @@ while True:
             robots[cur] = robots[cur - 1]
             robots[cur - 1] = 0
             # 내구도 약화
-            belt[(start + cur) % (2 * N)] -= 1
+            belt[cur] -= 1
+
     # 2-2. 로봇 내리기
     robots[N - 1] = 0
 
     # 3. 내구도 확인 후 상자 올리기
-    if belt[start] > 0:
+    if belt[0] > 0:
         robots[0] = 1
-        belt[start] -= 1
+        belt[0] -= 1
 
     # 4. 내구도 0인 자리 갯수 확인
     if belt.count(0) >= K:
