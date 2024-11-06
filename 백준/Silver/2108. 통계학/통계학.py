e335c6721538069bdb282b16901b20e54c75698c
YeJin Eum <139303917+yjeum@users.noreply.github.com>
2024-11-06 22:02:28 +0900

    [Silver III] Title: 통계학, Time: 432 ms, Memory: 52796 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.py
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -1,27 +1,31 @@
-N = int(input())
+import sys
 
-num_dic = {}
-num_lst = []
-sum = 0
+input = sys.stdin.readline
+
+N = int(input())
+nums = []
+nums_dic = {}
 max_cnt = 0
 
 for _ in range(N):
-    i = int(input())
-    sum += i
-    num_lst.append(i)
-    num_dic[i] = num_dic.get(i, 0) + 1
-    if max_cnt < num_dic[i]:
-        max_cnt = num_dic[i]
+    num = int(input())
+    nums.append(num)
+    nums_dic[num] = nums_dic.get(num, 0) + 1
+    if max_cnt < nums_dic[num]:
+        max_cnt = nums_dic[num]
+nums.sort()
 
-num_lst.sort()
-print(round(sum/N))
-print(num_lst[N//2])
+print(round(sum(nums) / N))
+print(nums[N // 2])
 
-max_val = [num for num, count in num_dic.items() if count == max_cnt]
+max_val = []
+for key, val in nums_dic.items():
+    if val == max_cnt:
+        max_val.append(key)
 max_val.sort()
 if len(max_val) > 1:
     print(max_val[1])
 else:
     print(max_val[0])
 
-print(num_lst[N-1] - num_lst[0])
+print(nums[-1] - nums[0])
