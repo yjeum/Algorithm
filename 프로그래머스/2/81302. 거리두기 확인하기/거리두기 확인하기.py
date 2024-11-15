f3ed604190cb01bbf6fa62fcb31b5481281d70b6
YeJin Eum <139303917+yjeum@users.noreply.github.com>
2024-11-15 22:16:48 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.37 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
@@ -0,0 +1,45 @@
+didj = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+
+def room(place):
+    for i in range(5):
+        for j in range(5):
+            if place[i][j] == 'P':
+                visited = [[0] * (5) for _ in range(5)]
+                visited[i][j] = 1
+                if recur(i, j, 0, place, visited) == False:
+                    return False
+    return True
+
+
+def recur(ci, cj, depth, place, visited):
+    
+    if depth == 2:
+        return True
+    
+    for di, dj in didj:
+        ni, nj = ci + di, cj + dj
+        if 0 <= ni < 5 and 0 <= nj < 5 and visited[ni][nj] == 0:
+            visited[ni][nj] = 1
+            print(ni, nj, place[ni][nj])
+            if place[ni][nj] == 'P':
+                print('in')
+                return False
+            elif place[ni][nj] == 'O':
+                if not recur(ni, nj, depth + 1, place, visited):
+                    return False
+    return True
+    
+    
+
+def solution(places):
+    answer = []
+    for t in range(5):
+        print(t)
+        if room(places[t]) == False:
+            print('start')
+            answer.append(0)
+            print(answer)
+        else:
+            answer.append(1)
+
+    return answer
