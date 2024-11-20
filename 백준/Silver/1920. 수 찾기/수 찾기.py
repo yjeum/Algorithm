99cd0166510b8e6e994580368c699ab3dfc42878
YeJin Eum <139303917+yjeum@users.noreply.github.com>
2024-11-20 15:49:41 +0900

    [Silver IV] Title: 수 찾기, Time: 172 ms, Memory: 52008 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,23 +1,6 @@
 import sys
+
 input = sys.stdin.readline
-
-
-def binary(target):
-
-    left, right = 0, len(N_lst) - 1
-
-    while left <= right:
-        mid = (left + right) // 2
-        if target < N_lst[mid]:
-            right = mid - 1
-        elif target == N_lst[mid]:
-            print(1)
-            return True
-        else:
-            left = mid + 1
-    print(0)
-    return False
-
 
 N = int(input())
 N_lst = list(map(int, input().split()))
@@ -25,7 +8,12 @@ N_lst = list(map(int, input().split()))
 M = int(input())
 M_lst = list(map(int, input().split()))
 
-N_lst.sort()
-
-for target in M_lst:
-    binary(target)
+num_dic = {}
+for number in N_lst:
+    num_dic[number] = num_dic.get(number, 0) + 1
+    
+for i in M_lst:
+    if i in num_dic:
+        print(1)
+    else:
+        print(0)
