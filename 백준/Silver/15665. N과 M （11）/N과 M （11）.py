e004d864c1ba03e0cf3ae2eb3e9e90d4c8e15f91
YeJin Eum <139303917+yjeum@users.noreply.github.com>
2024-11-27 14:55:33 +0900

    [Silver II] Title: N과 M (11), Time: 624 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15665. N과 M （11）/N과 M （11）.py
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.py
@@ -1,22 +1,22 @@
 import sys
 input = sys.stdin.readline
 
-def par(par_lst, num_lst):
-    if len(par_lst) == M:
-        print(*par_lst)
+
+def backtracking(lst):
+    if len(lst) == M:
+        print(*lst)
         return
-    
-    c_n = 0
-    for i in range(len(num_lst)):
-        if c_n != num_lst[i]:
-            par_lst.append(num_lst[i])
-            c_n = num_lst[i]
-            par(par_lst, num_lst)
-            par_lst.pop()
+
+    cur = 0
+    for num in num_lst:
+        if num != cur:
+            cur = num
+            lst.append(num)
+            backtracking(lst)
+            lst.pop()
+
 
 N, M = map(int, input().split())
 num_lst = list(map(int, input().split()))
 num_lst.sort()
-par_lst = []
-
-par(par_lst, num_lst)
+backtracking([])
