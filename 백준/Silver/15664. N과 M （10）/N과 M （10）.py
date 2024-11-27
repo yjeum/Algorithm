7bc97d2b978587d7d4789cb34190dd5950b9598c
YeJin Eum <139303917+yjeum@users.noreply.github.com>
2024-11-27 14:32:36 +0900

    [Silver II] Title: N과 M (10), Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15664. N과 M （10）/N과 M （10）.py
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -1,26 +1,28 @@
 import sys
 input = sys.stdin.readline
 
-def par(par_lst, num_lst, visited, c):
-    if len(par_lst) == M:
-        print(*par_lst)
+
+def backtracking(lst, order):
+    if len(lst) == M:
+        print(*lst)
         return
-    
-    c_n = 0
-    for i in range(c+1, len(num_lst)):
-        if visited[i]==0 and c_n != num_lst[i]:
-            par_lst.append(num_lst[i])
+
+    cur = 0
+    for i in range(order, N):
+        if num_lst[i] != cur and visited[i] == 0:
+
+            cur = num_lst[i]
             visited[i] = 1
-            c_n = num_lst[i]
-            par(par_lst, num_lst, visited, i)
-            par_lst.pop()
+            lst.append(cur)
+
+            backtracking(lst, i + 1)
+
+            lst.pop()
             visited[i] = 0
+
 
 N, M = map(int, input().split())
 num_lst = list(map(int, input().split()))
 num_lst.sort()
-par_lst = []
-total = []
-visited = [0]*N
-
-par(par_lst, num_lst, visited, -1)
+visited = [0] * N
+backtracking([], 0)
