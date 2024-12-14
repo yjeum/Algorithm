a7b42e85bebc4f367f64b39867ffa7048295a8d5
YeJin Eum <139303917+yjeum@users.noreply.github.com>
2024-12-14 16:39:57 +0900

    [Silver II] Title: 안녕, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1535. 안녕/안녕.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+# 총인원
+N = int(input())
+# 사용되는 체력, 얻는 기쁨 관련 리스트
+hp = [0] + list(map(int, input().split()))
+joy = [0] + list(map(int, input().split()))
+
+# knapsack 정보 업데이트
+knapsack = [[0] * (100) for _ in range(N + 1)]
+for i in range(1, N + 1):  # 만나는 사람
+    for j in range(1, 100):  # 사용된 체력
+
+        # 현재 만나는 사람만 인사했을 때에도 체력이 부족한 경우
+        if j < hp[i]:
+            knapsack[i][j] = knapsack[i - 1][j]
+        # 현재 만나는 사람과 인사가 가능한 경우우
+        else:
+            # 인사를 하는 경우, 하지 않는 경우 비교교
+            knapsack[i][j] = max(
+                knapsack[i - 1][j], joy[i] + knapsack[i - 1][j - hp[i]]
+            )
+
+print(knapsack[N][99])
