31e04c346ef08ac95f9f58d0327899583264182d
YeJin Eum <139303917+yjeum@users.noreply.github.com>
2025-12-11 19:30:54 +0900

    [Silver I] Title: 회전 초밥, Time: 3344 ms, Memory: 33904 KB -BaekjoonHub

---
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+N, d, k, c = map(int, input().split())
+
+num_list = [int(input()) for _ in range(N)]
+num_list.extend(num_list)
+
+max_num = 0
+for i in range(N):
+    cur_list = num_list[i : i + k] + [c]
+    max_num = max(len(set(cur_list)), max_num)
+
+print(max_num)
