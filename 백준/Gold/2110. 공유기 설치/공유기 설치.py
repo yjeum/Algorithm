49f6ea428e077312f55fb9baff31f04e13f26194
YeJin Eum <139303917+yjeum@users.noreply.github.com>
2026-04-22 20:55:30 +0900

    [Gold IV] Title: 공유기 설치, Time: 244 ms, Memory: 41164 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -2,22 +2,34 @@ import sys
 input = sys.stdin.readline
 
 N, C = map(int, input().split())
-house = list(int(input()) for _ in range(N))
-house.sort()
 
-start, end = 1, (house[-1] - house[0]) // (C - 1)
+house_list = [int(input()) for _ in range(N)]
+house_list.sort()
+
+# 거점 상관 없이 거리 상으로 균등하게 배분했을 경우(최상의 경우)
+start, end = 1, (house_list[N - 1] - house_list[0]) // (C - 1)
+
 while start <= end:
+
     mid = (start + end) // 2
-    cur = house[0]
-    cnt = 0
-    for i in house:
+
+    cur = house_list[0]
+    cnt = 1
+
+    for i in house_list:
+
+        # 생각한 거리 이상인 경우 설치
         if i - cur >= mid:
             cnt += 1
+
+            # 현재 위치 변경
             cur = i
-            if cnt >= C - 1:
+
+            # 이미 공유기를 다 설치했다면 생각한 거리보다 더 멀어도 설치 가능
+            if cnt >= C:
                 break
 
-    if cnt >= C - 1:
+    if cnt >= C:
         start = mid + 1
     else:
         end = mid - 1
