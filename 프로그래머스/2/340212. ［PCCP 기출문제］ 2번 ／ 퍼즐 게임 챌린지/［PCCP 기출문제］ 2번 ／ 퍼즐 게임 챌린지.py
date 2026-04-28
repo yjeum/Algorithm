1c924240421aef7cfd96a71ee57e59ae6965536a
YeJin Eum <139303917+yjeum@users.noreply.github.com>
2026-04-28 20:32:27 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 2323.19 ms, Memory: 33.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.py
@@ -0,0 +1,23 @@
+def solution(diffs, times, limit):
+    left, right = 1, limit
+    
+    while left < right:
+        mid = (left + right) // 2
+        
+        temp = 0
+        for i in range(len(diffs)):
+            if diffs[i] <= mid:
+                temp += times[i]
+            else:
+                temp += (diffs[i] - mid) * (times[i] + times[i-1]) + times[i]
+            
+            if temp > limit:
+                break
+        
+        if temp > limit:
+            left = mid + 1
+        else:
+            right = mid
+            
+    answer = right
+    return answer
