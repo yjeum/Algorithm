d9df646398e3d4df1b05de2861c953724c2cf18e
YeJin Eum <139303917+yjeum@users.noreply.github.com>
2026-09-26 22:46:31 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 2.79 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.py
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.py
@@ -29,3 +29,29 @@ def solution(mats, park):
                         return mat
     
     return -1
+
+# 다른 사람 풀이 > all 사용해서 한번에 확인
+# all과 any 사용법 익히기
+
+# def can_place_mat(park, size):
+#     # 공원의 행(row)와 열(col) 길이
+#     rows, cols = len(park), len(park[0])
+
+#     # park에서 주어진 size의 돗자리를 놓을 수 있는지 확인
+#     for i in range(rows - size + 1):
+#         for j in range(cols - size + 1):
+#             # size x size 크기의 공간이 모두 '-1'인지 확인
+#             if all(park[x][y] == '-1' for x in range(i, i + size) for y in range(j, j + size)):
+#                 return True
+#     return False
+
+# def solution(mats, park):
+#     # 돗자리 크기 내림차순으로 정렬
+#     mats.sort(reverse=True)
+
+#     # 각 돗자리 크기에 대해 놓을 수 있는지 확인
+#     for size in mats:
+#         if can_place_mat(park, size):
+#             return size
+
+#     return -1
